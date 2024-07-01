767e28b9500f1f726730e709d3ad55c83cea37be
Сафронов Евгений <emsafronov2004@gmail.com>
2024-07-02 01:15:14 +0300

    app

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,7 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:winfox/core/di/di.config.dart';
 
 final getIt = GetIt.instance;
 
 @InjectableInit()
-void confingureDependecies() {}
+void confingureDependecies() {
+  getIt.init();
+}
---
+++ lib/widgets/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Winfox test',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('ru'),
+        Locale('en'),
+      ],
+    );
+  }
+}
